7d55ff0a543c37519b57067adbe336135d945a0d
馥靈之鑰 Hour Light 芳香能量調頻 <judyanee@gmail.com>
2026-03-25 15:33:17 +0800

    Update hl-topnav.js

--- assets/js/hl-topnav.js
+++ assets/js/hl-topnav.js
@@ -56,7 +56,7 @@
   var NAV = [
     {group:'探索', items:[
       {label:'首頁', href:'index.html'},
-      {label:'二十四大命盤', href:'destiny-engine.html'},
+      {label:'三十三大命盤', href:'destiny-engine.html'},
       {label:'合盤配對', href:'destiny-match.html'},
       {label:'馥靈秘碼', href:'fuling-mima.html'},
       {label:'智慧抽牌', href:'draw-hl.html'},
